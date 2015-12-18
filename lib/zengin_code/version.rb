91c5d5314e585f97889afe1ff01e8090ca605411
Sho Kusano <rosylilly@aduca.org>
2015-12-19 07:59:34 +0900

    v1.0.1

--- lib/zengin_code/version.rb
+++ lib/zengin_code/version.rb
@@ -3,7 +3,7 @@ require 'pathname'
 module ZenginCode
   ROOT_DIR = Pathname.new(File.expand_path(__FILE__)).join('../../..')
   DATA_DIR = ROOT_DIR.join('source-data', 'data')
-  GEM_VERSION = '1.0.0'
+  GEM_VERSION = '1.0.1'
 
   def self.version
     "#{GEM_VERSION}-p#{File.read(DATA_DIR.join('updated_at')).strip}"
